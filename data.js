96370a975ecec4b0406de1fb9fb23d7f0e45e4db
AHMED <ahmed@AHMEDs-MacBook-Pro.local>
2024-08-23 14:46:04 +0300

    add data seeding

---
+++ data.js
@@ -0,0 +1,71 @@
+const books = [
+  {
+    title: "Black Swan",
+    author: "66bc239214d63ce622f6b44a",
+    description: "About Black Swan",
+    price: 10,
+    cover: "soft cover",
+  },
+  {
+    title: "Skin In The Game",
+    author: "66bc239214d63ce622f6b44a",
+    description: "About Skin In The Game",
+    price: 12,
+    cover: "soft cover",
+  },
+  {
+    title: "Fooled By Randomness",
+    author: "66bc239214d63ce622f6b44a",
+    description: "About Fooled By Randomness",
+    price: 8,
+    cover: "hard cover",
+  },
+  {
+    title: "The Forty Rules Of Loves",
+    author: "66bf0f46521d1f2b7302cf63",
+    description: "About The Forty Rules Of Loves",
+    price: 14,
+    cover: "soft cover",
+  },
+  {
+    title: "The Island Of Missing Trees",
+    author: "66bf0f46521d1f2b7302cf63",
+    description: "About The Island Of Missing Trees",
+    price: 9,
+    cover: "soft cover",
+  },
+  {
+    title: "The Flea Palace",
+    author: "66bf0f46521d1f2b7302cf63",
+    description: "About The Flea Palace",
+    price: 10,
+    cover: "hard cover",
+  },
+];
+
+const authors = [
+  {
+    firstName: "Robert",
+    lastName: "Kiyosaki",
+    nationality: "USA",
+  },
+  {
+    firstName: "Paulo",
+    lastName: "Coelho",
+    nationality: "Brazil",
+  },
+  {
+    firstName: "Wayne",
+    lastName: "Dyer",
+    nationality: "USA",
+  },
+  {
+    firstName: "Rolf",
+    lastName: "Dobelli",
+    nationality: "Switzerland",
+  },
+];
+
+module.exports = {
+  books,
+};
---
+++ seeder.js
@@ -0,0 +1,35 @@
+const { Book } = require("./models/Book");
+const { books } = require("./data");
+const connectToDo = require("./config/db");
+require("dotenv").config();
+
+// Connection TO DB
+connectToDo();
+
+// Import Books
+const importBook = async () => {
+  try {
+    await Book.insertMany(books);
+    console.log("books Imported");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// Remove Book
+const removeBooks = async () => {
+  try {
+    await Book.deleteMany();
+    console.log("books Removed");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-import") {
+  importBook();
+} else if (process.argv[2] === "-remove") {
+  removeBooks();
+}
